70520bd7c0cb0910e4da6c23e1755fdf6d300bb3
gty <1251890862@qq.com>
2023-05-07 12:19:03 +0800

    my first work

---
+++ code.cpp
@@ -0,0 +1,221 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
+int  Visited[20];
+int  visited[20];
+int  Visited1[20];
+
+typedef struct ArcNode { //�ߣ������������Ͷ���
+    int  adjvex;   //�ߣ���������һ������������е�λ��
+    ArcNode* nextarc;//ָ����һ�ߣ���������ָ��  
+}ArcNode;
+typedef struct Vnode {//�����㼰����������Ͷ���
+    string data;    //������Ϣ
+    ArcNode* firstarc;    //ָ������ö���ıߣ���������
+} Vnode, AdjList[10];
+typedef struct {
+    AdjList  vertices;
+    int  vexnum, arcnum;    //ͼ�ĵ�ǰ�������ͻ���
+} ALGraph;    //ͼ�ڽӱ�����
+typedef struct
+{
+    int elem[20];
+    int top;
+}seqstack;
+void initStack(seqstack& s)
+{
+    s.top = 0;
+}
+void push(seqstack& s, int e)
+{
+    s.elem[s.top] = e;
+    s.top++;
+}
+int pop(seqstack& s, int& e)
+{
+    s.top--;
+    return   e = s.elem[s.top];
+}
+int stackEmpty(seqstack s)
+{
+    if (s.top == 0)
+        return 1;
+    else return 0;
+}
+
+int  LocateVex(ALGraph& G, string vp)
+{
+    int  k = 0;
+    for (k = 0; k < G.vexnum; k++)
+    {
+        if (G.vertices[k].data == vp)
+            return(k);
+    }
+    return(-1);     /*  ͼ���޴˶���  */
+}
+void CreatALGraph(ALGraph& G)//����ͼ�Ĵ���
+{
+    string v1, v2;
+    int i, j, k;
+    ArcNode* p1, * p2, * p, * q;
+    printf("���붥�����ͱ���\n");
+    cin >> G.vexnum >> G.arcnum;
+    for (i = 0; i < G.vexnum; i++)
+    {
+        printf("�����붥����Ϣ\n");
+        cin >> G.vertices[i].data;
+        G.vertices[i].firstarc = NULL;
+    }
+    for (k = 0; k < G.arcnum; k++)
+    {
+        printf("��������ͨ�ı�\n");
+        cin >> v1 >> v2;
+        i = LocateVex(G, v1);
+        j = LocateVex(G, v2);
+        p1 = new ArcNode;
+        p1->adjvex = j;
+        p1->nextarc = NULL;
+        p = G.vertices[i].firstarc;
+        if (p == NULL)
+            G.vertices[i].firstarc = p1;
+        else
+        {
+            while (p->nextarc)
+            {
+                p = p->nextarc;
+            }
+            p->nextarc = p1;
+        }
+        p2 = new ArcNode;
+        p2->adjvex = i;
+        p2->nextarc = NULL;
+        q = G.vertices[j].firstarc;
+        if (q == NULL)
+            G.vertices[j].firstarc = p2;
+        else
+        {
+            while (q->nextarc)
+            {
+                q = q->nextarc;
+            }
+            q->nextarc = p2;
+        }
+    }
+}
+
+
+int FirstAdjvex(ALGraph G, int u)
+{
+    ArcNode* t;
+    t = G.vertices[u].firstarc;
+    if (t != NULL)
+        return t->adjvex;
+    else
+        return -1;
+}
+int NextAdjvex(ALGraph G, int u, int w)
+{
+    ArcNode* t;
+    t = G.vertices[u].firstarc;
+    while (t != NULL)
+    {
+        if (t->adjvex != w)
+            t = t->nextarc;
+        else if (t->nextarc != NULL)
+            return t->nextarc->adjvex;
+        else
+            return -1;
+    }
+    return -1;
+}
+void BFS(ALGraph G, int v)
+{
+    seqstack q;
+    int u, w;
+    cout << G.vertices[v].data << " ";
+    Visited1[v] = true;
+    initStack(q);
+    push(q, v);
+    while (!stackEmpty(q))
+    {
+        pop(q, u);
+        for (w = FirstAdjvex(G, u); w >= 0; w = NextAdjvex(G, u, w))
+            if (!Visited1[w])
+            {
+                cout << G.vertices[w].data << " ";
+                Visited1[w] = true;
+                push(q, w);
+            }
+    }
+    cout << endl;
+}
+
+void DFS1(ALGraph G, int v)
+{//������������ݹ��㷨 
+    ArcNode *p;
+    visited[v] = 1;
+    cout<<G.vertices[v].data<<" ";
+    p = G.vertices[v].firstarc;
+    while (p)
+    {
+        if (!visited[p->adjvex])
+            DFS1(G, p->adjvex);
+        p = p->nextarc;
+    }
+}
+
+
+
+
+void DFS(ALGraph G, int v)
+{
+    seqstack s;
+    int k, w;
+    initStack(s);
+    push(s, v);
+    while (!stackEmpty(s))
+    {
+        pop(s, k);
+        if (!Visited[k])
+        {
+            Visited[k] = true; cout << G.vertices[k].data << " ";
+            for (w = FirstAdjvex(G, k); w >= 0; w = NextAdjvex(G, k, w))
+            {
+                if (!Visited[w])
+                {
+                    push(s, w);
+                }
+            }
+        }
+    }
+    cout << endl;
+}
+int main()
+{
+    int  flag1 = 1;
+    while (flag1)
+    {
+        ALGraph G;
+        CreatALGraph(G);
+        int flag = 1, i, j;
+        printf("����0������\n");
+        printf("����1����ͼDFS\n");
+        printf("����2����ͼBFS\n");
+        while (flag)
+        {
+            printf("������0��1 ��2\n");
+            cin >> i;
+            if (i == 1)   DFS(G, 0);
+            if (i == 2)   BFS(G, 0);
+            if (i == 0)   break;
+
+        }
+        printf("����1���¹���������ͨͼ��������0��������\n");
+        cin >> j;
+        if (j == 0)  break;
+    }
+    return 0;
+}
+ 
